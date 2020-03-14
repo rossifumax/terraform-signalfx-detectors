1c9b84235bd3032fe038bf0efaca4bc2f7ddc20c
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-03-14 09:18:03 -0700

    Create variables.tf

---
+++ cloud/aws/apigateway/variables.tf
@@ -0,0 +1,261 @@
+# Global
+
+variable "environment" {
+  description = "Infrastructure environment"
+  type        = string
+}
+
+# SignalFx module specific
+
+variable "notifications" {
+  description = "Notification recipients list for every detectors"
+  type        = list
+}
+
+variable "prefixes" {
+  description = "Prefixes list to prepend between brackets on every monitors names before environment"
+  type        = list
+  default     = []
+}
+
+variable "filter_custom_includes" {
+  description = "List of tags to include when custom filtering is used"
+  type        = list
+  default     = []
+}
+
+variable "filter_custom_excludes" {
+  description = "List of tags to exclude when custom filtering is used"
+  type        = list
+  default     = []
+}
+
+variable "detectors_disabled" {
+  description = "Disable all detectors in this module"
+  type        = bool
+  default     = false
+}
+
+# AWS ApiGateway detectors specific
+
+variable "heartbeat_disabled" {
+  description = "Disable all alerting rules for heartbeat detector"
+  type        = bool
+  default     = null
+}
+
+variable "heartbeat_notifications" {
+  description = "Notification recipients list for every alerting rules of heartbeat detector"
+  type        = list
+  default     = []
+}
+
+variable "heartbeat_timeframe" {
+  description = "Timeframe for system not reporting detector (i.e. \"10m\")"
+  type        = string
+  default     = "20m"
+}
+
+# Latency detectors
+
+variable "latency_disabled" {
+  description = "Disable all alerting rules for latency detector"
+  type        = bool
+  default     = null
+}
+
+variable "latency_disabled_critical" {
+  description = "Disable critical alerting rule for latency detector"
+  type        = bool
+  default     = null
+}
+
+variable "latency_disabled_warning" {
+  description = "Disable warning alerting rule for latency detector"
+  type        = bool
+  default     = null
+}
+
+variable "latency_notifications" {
+  description = "Notification recipients list for every alerting rules of latency detector"
+  type        = list
+  default     = []
+}
+
+variable "latency_notifications_warning" {
+  description = "Notification recipients list for warning alerting rule of latency detector"
+  type        = list
+  default     = []
+}
+
+variable "latency_notifications_critical" {
+  description = "Notification recipients list for critical alerting rule of latency detector"
+  type        = list
+  default     = []
+}
+
+variable "latency_aggregation_function" {
+  description = "Aggregation function and group by for latency detector (i.e. \".mean(by=['host'])\")"
+  type        = string
+  default     = ".mean(by=['aws_region'])"
+}
+
+variable "latency_transformation_function" {
+  description = "Transformation function for latency detector (mean, min, max)"
+  type        = string
+  default     = "min"
+}
+
+variable "latency_transformation_window" {
+  description = "Transformation window for latency detector (i.e. 5m, 20m, 1h, 1d)"
+  type        = string
+  default     = "5m"
+}
+
+variable "latency_threshold_critical" {
+  description = "Critical threshold for latency detector"
+  type        = number
+  default     = 3000
+}
+
+variable "latency_threshold_warning" {
+  description = "Warning threshold for latency detector"
+  type        = number
+  default     = 1000
+}
+
+# Httpcode_5xx_errors detectors
+
+variable "httpcode_5xx_errors_disabled" {
+  description = "Disable all alerting rules for httpcode 5xx errors detector"
+  type        = bool
+  default     = null
+}
+
+variable "httpcode_5xx_errors_disabled_critical" {
+  description = "Disable critical alerting rule for httpcode 5xx errors detector"
+  type        = bool
+  default     = null
+}
+
+variable "httpcode_5xx_errors_disabled_warning" {
+  description = "Disable warning alerting rule for httpcode 5xx erros detector"
+  type        = bool
+  default     = null
+}
+
+variable "httpcode_5xx_errors_notifications" {
+  description = "Notification recipients list for every alerting rules of httpcode 5xx errors detector"
+  type        = list
+  default     = []
+}
+
+variable "httpcode_5xx_errors_notifications_warning" {
+  description = "Notification recipients list for warning alerting rule of httpcode 5xx errors detector"
+  type        = list
+  default     = []
+}
+
+variable "httpcode_5xx_errors_notifications_critical" {
+  description = "Notification recipients list for critical alerting rule of httpcode 5xx errors detector"
+  type        = list
+  default     = []
+}
+
+variable "httpcode_5xx_errors_aggregation_function" {
+  description = "Aggregation function and group by for httpcode 5xx errors detector (i.e. \".mean(by=['host'])\")"
+  type        = string
+  default     = ".mean(by=['aws_region'])"
+}
+
+variable "httpcode_5xx_errors_transformation_function" {
+  description = "Transformation function for httpcode 5xx errors detector (mean, min, max)"
+  type        = string
+  default     = "min"
+}
+
+variable "httpcode_5xx_errors_transformation_window" {
+  description = "Transformation window for httpcode 5xx errors detector (i.e. 5m, 20m, 1h, 1d)"
+  type        = string
+  default     = "5m"
+}
+
+variable "httpcode_5xx_errors_threshold_critical" {
+  description = "Critical threshold for httpcode 5xx errors detector"
+  type        = number
+  default     = 20
+}
+
+variable "httpcode_5xx_errors_threshold_warning" {
+  description = "Warning threshold for httpcode 5xx errors detector"
+  type        = number
+  default     = 10
+}
+
+# Httpcode_4xx_errors detectors
+
+variable "httpcode_4xx_errors_disabled" {
+  description = "Disable all alerting rules for httpcode 4xx errors detector"
+  type        = bool
+  default     = null
+}
+
+variable "httpcode_4xx_errors_disabled_critical" {
+  description = "Disable critical alerting rule for httpcode 4xx errors detector"
+  type        = bool
+  default     = null
+}
+
+variable "httpcode_4xx_errors_disabled_warning" {
+  description = "Disable warning alerting rule for httpcode 4xx errors detector"
+  type        = bool
+  default     = null
+}
+
+variable "httpcode_4xx_errors_notifications" {
+  description = "Notification recipients list for every alerting rules of httpcode 4xx errors detector"
+  type        = list
+  default     = []
+}
+
+variable "httpcode_4xx_errors_notifications_warning" {
+  description = "Notification recipients list for warning alerting rule of httpcode 4xx errors detector"
+  type        = list
+  default     = []
+}
+
+variable "httpcode_4xx_errors_notifications_critical" {
+  description = "Notification recipients list for critical alerting rule of httpcode 4xx errors detector"
+  type        = list
+  default     = []
+}
+
+variable "httpcode_4xx_errors_aggregation_function" {
+  description = "Aggregation function and group by for httpcode 4xx errors detector (i.e. \".mean(by=['host'])\")"
+  type        = string
+  default     = ".mean(by=['aws_region'])"
+}
+
+variable "httpcode_4xx_errors_transformation_function" {
+  description = "Transformation function for httpcode 4xx errors detector (mean, min, max)"
+  type        = string
+  default     = "min"
+}
+
+variable "httpcode_4xx_errors_transformation_window" {
+  description = "Transformation window for httpcode 4xx errors detector (i.e. 5m, 20m, 1h, 1d)"
+  type        = string
+  default     = "5m"
+}
+
+variable "httpcode_4xx_errors_threshold_critical" {
+  description = "Critical threshold for httpcode 4xx errors detector"
+  type        = number
+  default     = 30
+}
+
+variable "httpcode_4xx_errors_threshold_warning" {
+  description = "Warning threshold for httpcode 4xx errors detector"
+  type        = number
+  default     = 15
+}

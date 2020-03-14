c8fa7b2ef2d82fdc528bf3131fabc20d1c9178ee
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-03-14 10:38:58 -0700

    Update monitors-ecs-cluster.tf

--- cloud/aws/ecs/ec2-cluster/monitors-ecs-cluster.tf
+++ cloud/aws/ecs/ec2-cluster/monitors-ecs-cluster.tf
@@ -1,20 +1,20 @@
 resource "signalfx_detector" "heartbeat" {
-  name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] AWS ECS cluster heartbeat"
+	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] AWS ECS cluster heartbeat"
 
-  program_text = <<-EOF
+	program_text = <<-EOF
 		from signalfx.detectors.not_reporting import not_reporting
 		signal = data('CPUUtilization', filter=filter('stat', 'mean') and filter('namespace', 'AWS/ECS') and ${module.filter-tags.filter_custom})
 		not_reporting.detector(stream=signal, resource_identifier=['host'], duration='${var.heartbeat_timeframe}').publish('CRIT')
-  EOF
+	EOF
 
-  rule {
-	description           = "has not reported in ${var.heartbeat_timeframe}"
-	severity              = "Critical"
-	detect_label          = "CRIT"
-	disabled              = coalesce(var.heartbeat_disabled, var.detectors_disabled)
-	notifications         = coalescelist(var.heartbeat_notifications, var.notifications)
-	parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{ruleName}}} on {{{dimensions}}}"
-  }
+	rule {
+		description           = "has not reported in ${var.heartbeat_timeframe}"
+		severity              = "Critical"
+		detect_label          = "CRIT"
+		disabled              = coalesce(var.heartbeat_disabled, var.detectors_disabled)
+		notifications         = coalescelist(var.heartbeat_notifications, var.notifications)
+		parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{ruleName}}} on {{{dimensions}}}"
+	}
 }
 
 resource "signalfx_detector" "cpu_utilization" {

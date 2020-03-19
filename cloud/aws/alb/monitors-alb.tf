3b322f1916ba4b64da6f0b6728405473191865b0
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-03-19 13:18:40 -0700

    Update monitors-alb.tf

--- cloud/aws/alb/monitors-alb.tf
+++ cloud/aws/alb/monitors-alb.tf
@@ -53,8 +53,8 @@ resource "signalfx_detector" "latency" {
 
 	program_text = <<-EOF
 		signal = data('TargetResponseTime', filter=filter('namespace', 'AWS/ApplicationELB') and filter('stat', 'mean') and ${module.filter-tags.filter_custom})${var.latency_aggregation_function}.${var.latency_transformation_function}(over='${var.latency_transformation_window}')
-		above_or_below_detector(signal, ${var.latency_threshold_critical}, lasting('${var.latency_aperiodic_duration}', ${var.latency_aperiodic_percentage})).publish('CRIT')
-		above_or_below_detector(signal, ${var.latency_threshold_warning}, lasting('${var.latency_aperiodic_duration}', ${var.latency_aperiodic_percentage})).publish('WARN')
+		above_or_below_detector(signal, ${var.latency_threshold_critical}, 'above', lasting('${var.latency_aperiodic_duration}', ${var.latency_aperiodic_percentage})).publish('CRIT')
+		above_or_below_detector(signal, ${var.latency_threshold_warning}, 'above', lasting('${var.latency_aperiodic_duration}', ${var.latency_aperiodic_percentage})).publish('WARN')
 	EOF
 
 	rule {

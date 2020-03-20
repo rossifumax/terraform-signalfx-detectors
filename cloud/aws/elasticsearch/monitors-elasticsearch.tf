7fe07a3b4bea8da1f12204ffc51fc45f2a77ca58
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-03-20 05:52:02 -0700

    Update monitors-elasticsearch.tf

--- cloud/aws/elasticsearch/monitors-elasticsearch.tf
+++ cloud/aws/elasticsearch/monitors-elasticsearch.tf
@@ -21,7 +21,9 @@ resource "signalfx_detector" "cluster_status" {
 	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] ElasticSearch cluster status"
 
 	program_text = <<-EOF
-		signal = data('gauge.cluster.status', filter=filter('plugin', 'elasticsearch') and ${module.filter-tags.filter_custom})${var.cluster_status_aggregation_function}.${var.cluster_status_transformation_function}(over='${var.cluster_status_transformation_window}')
+		A = data('ClusterStatus.red', filter=filter('namespace', 'AWS/ES') and ${module.filter-tags.filter_custom})${var.cluster_status_aggregation_function}
+		B = data('ClusterStatus.yellow', filter=filter('namespace', 'AWS/ES') and ${module.filter-tags.filter_custom})${var.cluster_status_aggregation_function}
+		signal = (A*2)+(B+0.1).${var.cluster_status_transformation_function}(over='${var.cluster_status_transformation_window}')
 		detect(when(signal >= ${var.cluster_status_threshold_critical})).publish('CRIT')
 		detect(when(signal >= ${var.cluster_status_threshold_warning})).publish('WARN')
 	EOF
@@ -46,11 +48,41 @@ resource "signalfx_detector" "cluster_status" {
 
 }
 
+resource "signalfx_detector" "free_space" {
+	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] ElasticSearch cluster free storage space"
+
+	program_text = <<-EOF
+		A = data('FreeStorageSpace', filter=filter('namespace', 'AWS/ES')and ${module.filter-tags.filter_custom})${var.free_space_aggregation_function}
+		signal = ((A/${var.free_space_volume})*100).${var.free_space_transformation_function}(over='${var.free_space_transformation_window}')
+		detect(when(signal < ${var.free_space_threshold_critical})).publish('CRIT')
+		detect(when(signal < ${var.free_space_threshold_warning})).publish('WARN')
+	EOF
+
+	rule {
+		description           = "is too low < ${var.free_space_threshold_critical}"
+		severity              = "Critical"
+		detect_label          = "CRIT"
+		disabled              = coalesce(var.free_space_disabled_critical, var.free_space_disabled, var.detectors_disabled)
+		notifications         = coalescelist(var.free_space_notifications_critical, var.free_space_notifications, var.notifications)
+		parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{ruleName}}} ({{inputs.signal.value}}) on {{{dimensions}}}"
+	}
+
+	rule {
+		description           = "is too low < ${var.free_space_threshold_warning}"
+		severity              = "Warning"
+		detect_label          = "WARN"
+		disabled              = coalesce(var.free_space_disabled_warning, var.free_space_disabled, var.detectors_disabled)
+		notifications         = coalescelist(var.free_space_notifications_warning, var.free_space_notifications, var.notifications)
+		parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{ruleName}}} ({{inputs.signal.value}}) on {{{dimensions}}}"
+	}
+
+}
+
 resource "signalfx_detector" "cpu_90_15min" {
 	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] ElasticSearch cluster CPU"
 
 	program_text = <<-EOF
-		signal = data('gauge.process.cpu.percent', filter=filter('plugin', 'elasticsearch')and ${module.filter-tags.filter_custom})${var.cpu_90_15min_aggregation_function}.${var.cpu_90_15min_transformation_function}(over='${var.cpu_90_15min_transformation_window}')
+		signal = data('CPUUtilization', filter=filter('namespace', 'AWS/ES')and ${module.filter-tags.filter_custom})${var.cpu_90_15min_aggregation_function}.${var.cpu_90_15min_transformation_function}(over='${var.cpu_90_15min_transformation_window}')
 		detect(when(signal > ${var.cpu_90_15min_threshold_critical})).publish('CRIT')
 		detect(when(signal > ${var.cpu_90_15min_threshold_warning})).publish('WARN')
 	EOF

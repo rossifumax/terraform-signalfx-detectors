798ddc4775018e84dd4a63ecbe89f132ef1b735c
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-03-23 13:32:31 -0700

    Update monitors-gce-instance.tf

--- cloud/gcp/gce/instance/monitors-gce-instance.tf
+++ cloud/gcp/gce/instance/monitors-gce-instance.tf
@@ -1,3 +1,22 @@
+resource "signalfx_detector" "heartbeat" {
+	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] AWS Alb heartbeat"
+
+	program_text = <<-EOF
+		from signalfx.detectors.not_reporting import not_reporting
+		signal = data('instance/cpu/usage_time' and ${module.filter-tags.filter_custom})
+		not_reporting.detector(stream=signal, resource_identifier=['instance_name'], duration='${var.heartbeat_timeframe}').publish('CRIT')
+	EOF
+
+	rule {
+		description           = "has not reported in ${var.heartbeat_timeframe}"
+		severity              = "Critical"
+		detect_label          = "CRIT"
+		disabled              = coalesce(var.heartbeat_disabled, var.detectors_disabled)
+		notifications         = coalescelist(var.heartbeat_notifications, var.notifications)
+		parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{ruleName}}} on {{{dimensions}}}"
+	}
+}
+
 resource "signalfx_detector" "cpu_utilization" {
 	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] Compute Engine instance CPU utilization"
 

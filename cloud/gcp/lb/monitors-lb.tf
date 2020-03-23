76d55ecbd62f5683e9cb978c76209a0eb2e6ed65
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-03-23 13:36:08 -0700

    Update monitors-lb.tf

--- cloud/gcp/lb/monitors-lb.tf
+++ cloud/gcp/lb/monitors-lb.tf
@@ -1,3 +1,22 @@
+resource "signalfx_detector" "heartbeat" {
+	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] AWS Alb heartbeat"
+
+	program_text = <<-EOF
+		from signalfx.detectors.not_reporting import not_reporting
+		signal = data('https/total_latencies' and ${module.filter-tags.filter_custom})
+		not_reporting.detector(stream=signal, resource_identifier=['backend_name'], duration='${var.heartbeat_timeframe}').publish('CRIT')
+	EOF
+
+	rule {
+		description           = "has not reported in ${var.heartbeat_timeframe}"
+		severity              = "Critical"
+		detect_label          = "CRIT"
+		disabled              = coalesce(var.heartbeat_disabled, var.detectors_disabled)
+		notifications         = coalescelist(var.heartbeat_notifications, var.notifications)
+		parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{ruleName}}} on {{{dimensions}}}"
+	}
+}
+
 resource "signalfx_detector" "error_rate_4xx" {
 	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] GCP LB 4xx errors"
 

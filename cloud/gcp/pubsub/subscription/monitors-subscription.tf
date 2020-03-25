604b17ed2d03eaefd9ce627c0d6cb13e14a2083c
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-03-25 15:04:47 -0700

    Update monitors-subscription.tf

--- cloud/gcp/pubsub/subscription/monitors-subscription.tf
+++ cloud/gcp/pubsub/subscription/monitors-subscription.tf
@@ -3,7 +3,7 @@ resource "signalfx_detector" "heartbeat" {
 
 	program_text = <<-EOF
 		from signalfx.detectors.not_reporting import not_reporting
-		signal = data('subscription/pull_request_count' and ${module.filter-tags.filter_custom})
+		signal = data('subscription/pull_request_count' and ${module.filter-tags.filter_custom}).publish('signal')
 		not_reporting.detector(stream=signal, resource_identifier=['subscription_id'], duration='${var.heartbeat_timeframe}').publish('CRIT')
 	EOF
 
@@ -21,7 +21,7 @@ resource "signalfx_detector" "oldest_unacked_message" {
 	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] Pub/Sub subscription oldest unacknowledged message"
 
 	program_text = <<-EOF
-		signal = data('subscription/oldest_unacked_message_age', filter=filter('monitored_resource', 'pubsub_subscription') and ${module.filter-tags.filter_custom})${var.oldest_unacked_message_aggregation_function}.${var.oldest_unacked_message_transformation_function}(over='${var.oldest_unacked_message_transformation_window}')
+		signal = data('subscription/oldest_unacked_message_age', filter=filter('monitored_resource', 'pubsub_subscription') and ${module.filter-tags.filter_custom})${var.oldest_unacked_message_aggregation_function}.${var.oldest_unacked_message_transformation_function}(over='${var.oldest_unacked_message_transformation_window}').publish('signal')
 		detect(when(signal >= ${var.oldest_unacked_message_threshold_critical})).publish('CRIT')
 		detect(when(signal >= ${var.oldest_unacked_message_threshold_warning})).publish('WARN')
 	EOF
@@ -50,7 +50,7 @@ resource "signalfx_detector" "push_latency" {
 	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] Pub/Sub subscription latency on push endpoint"
 
 	program_text = <<-EOF
-		signal = data('subscription/push_request_latencies', filter=filter('monitored_resource', 'pubsub_subscription') and ${module.filter-tags.filter_custom})${var.push_latency_aggregation_function}.${var.push_latency_transformation_function}(over='${var.push_latency_transformation_window}')
+		signal = data('subscription/push_request_latencies', filter=filter('monitored_resource', 'pubsub_subscription') and ${module.filter-tags.filter_custom})${var.push_latency_aggregation_function}.${var.push_latency_transformation_function}(over='${var.push_latency_transformation_window}').publish('signal')
 		detect(when(signal >= ${var.push_latency_threshold_critical})).publish('CRIT')
 		detect(when(signal >= ${var.push_latency_threshold_warning})).publish('WARN')
 	EOF
